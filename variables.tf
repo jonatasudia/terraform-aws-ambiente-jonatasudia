e06fe98ea3bf47f6257c571af75a4c937fc71cc6
Jonatas C. Barbosa <jcandido@tribanco.com.br>
2024-12-16 20:11:55 -0300

    Primeiro commit

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "name" {
+  type        = string
+  description = "Nome da instancia"
+}
+
+variable "environment" {
+  type        = string
+  description = "Environment"
+  default     = "dev"
+}
+
+variable "ebs_block_device" {
+  description = "Lista de volumes ebs para ser criado e montado na instancia"
+  type        = list(any)
+}
